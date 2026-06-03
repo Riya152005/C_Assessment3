81b1ecc44bfea456ca89523618d0f96dc8836b8e
Riya152005 <riyarathna1507@gmail.com>
2026-06-03 17:44:13 +0530

    Create C_Programming_7.c

---
+++ C_Programming_7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num,x,y;
+	printf("Enter 2 digit number:");
+	scanf("%d",&num);
+	x=num/10;
+	y=num%10;
+	if(x==y)
+		printf("Success");
+	else
+		printf("Failure");
+}
